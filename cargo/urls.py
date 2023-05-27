8234a4734c91b95d7f9d6f3a1d38fa3c1bacdb36
CaDiBob <nikolay.v.byankin@gmail.com>
2023-05-27 22:06:03 +0900

    refactor: fixed database queries

--- cargo/urls.py
+++ cargo/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from cargo import views
 
 urlpatterns = [
-    path('cargo-list/', views.CargoInfo.as_view()),
-    path('add-cargo/', views.CreateCargoView.as_view()),
+    path('cargo/<int:pk>', views.DestroyUpdateCargoView.as_view()),
+    path('cargo-info/<int:pk>', views.CargoViewSet.as_view({'get': 'retrieve'})),
+    path('cargo-list/', views.CargoViewSet.as_view({'get': 'list'})),
+    path('add-cargo/', views.CargoViewSet.as_view({'post': 'create'})),
 ]
